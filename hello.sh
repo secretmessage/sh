a8c09991b13c6bbee0bf1ee50680265f5bf3bab8
mc <michaelchristophernewyork@hotmail.com>
2017-05-31 16:09:58 +0000

    update to use node 8

--- hello.sh
+++ hello.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get install git -yqq
 apt-get update
 apt-get -y upgrade
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
